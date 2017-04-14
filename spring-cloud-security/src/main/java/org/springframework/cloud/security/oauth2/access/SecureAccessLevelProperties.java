0064c8bdd5b54444d633099cb1c8bbd7c671bf38
kevindahub <kevin.van.houtte1@gmail.com>
2017-04-14 10:25:49 +0200

    Configuring Access Level of Downstream Endpoints on Zuul Proxy
    To increase performance, you can choose to implement a configuration security mechanism for making less network hops,
    
    a mechanism implemented on Zuul where you can configure which route needs private, public or partial authentication. +
    Every route in Zuul to a downstream service will have security configured based on how secure the endpoints has to be.

---
+++ spring-cloud-security/src/main/java/org/springframework/cloud/security/oauth2/access/SecureAccessLevelProperties.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.security.oauth2.access;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * @author Kevin Van Houtte
+ *
+ */
+@ConfigurationProperties("secure-access-level")
+public class SecureAccessLevelProperties {
+
+	/**
+	 * Access strategy per route.
+	 */
+	@Getter
+	@Setter
+	private Map<String, Route> routes = new LinkedHashMap<String, Route>();
+	
+	@Getter
+	@Setter
+	private boolean loadBalanced;
+
+	@PostConstruct
+	public void init() {
+		for (Entry<String, Route> entry : routes.entrySet()) {
+			if (entry.getValue().getId() == null) {
+				entry.getValue().setId(entry.getKey());
+			}
+		}
+	}
+
+	@Data
+	@NoArgsConstructor
+	public static class Route {
+		/**
+		 * The id of the route (e.g. discovery virtual hostname).
+		 */
+		private String id;
+		/**
+		 * The authentication scheme to use (e.g. "oauth2", "none").
+		 */
+		private String access;
+
+		public Route(String access) {
+			this.access = access;
+		}
+	}
+
+}
