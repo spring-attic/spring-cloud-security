e80e1e4231410dc36af1fc8a361fe4cd88b51f6d
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-26 03:49:15 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed But Review Recommended
    These URLs were fixed, but the https status was not OK. However, the https status was the same as the http request or http redirected to an https URL, so they were migrated. Your review is recommended.
    
    * [ ] http://compose.docker.io/ (UnknownHostException) with 1 occurrences migrated to:
      https://compose.docker.io/ ([https](https://compose.docker.io/) result UnknownHostException).
    * [ ] http://cloud.spring.io/spring-cloud.html (404) with 1 occurrences migrated to:
      https://cloud.spring.io/spring-cloud.html ([https](https://cloud.spring.io/spring-cloud.html) result 404).
    * [ ] http://example.com/test (404) with 2 occurrences migrated to:
      https://example.com/test ([https](https://example.com/test) result 404).
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/ with 1 occurrences migrated to:
      https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/ ([https](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/) result 200).
    * [ ] http://example.com with 4 occurrences migrated to:
      https://example.com ([https](https://example.com) result 200).
    * [ ] http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html with 1 occurrences migrated to:
      https://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html ([https](https://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html) result 200).
    * [ ] http://plugins.jetbrains.com/plugin/6546 with 1 occurrences migrated to:
      https://plugins.jetbrains.com/plugin/6546 ([https](https://plugins.jetbrains.com/plugin/6546) result 301).
    * [ ] http://eclipse.org with 1 occurrences migrated to:
      https://eclipse.org ([https](https://eclipse.org) result 302).
    * [ ] http://eclipse.org/m2e/ with 2 occurrences migrated to:
      https://eclipse.org/m2e/ ([https](https://eclipse.org/m2e/) result 302).
    * [ ] http://www.springsource.com/developer/sts with 1 occurrences migrated to:
      https://www.springsource.com/developer/sts ([https](https://www.springsource.com/developer/sts) result 302).
    
    # Ignored
    These URLs were intentionally ignored.
    
    * http://nosuchservice with 2 occurrences

--- spring-cloud-security/src/test/java/org/springframework/cloud/security/oauth2/client/ResourceServerTokenRelayAutoConfigurationTests.java
+++ spring-cloud-security/src/test/java/org/springframework/cloud/security/oauth2/client/ResourceServerTokenRelayAutoConfigurationTests.java
@@ -62,7 +62,7 @@ public class ResourceServerTokenRelayAutoConfigurationTests {
 	public void clientNotConfigured() {
 		this.context = new SpringApplicationBuilder(NoClientConfiguration.class)
 				.properties("spring.config.name=test", "server.port=0",
-						"security.oauth2.resource.userInfoUri:http://example.com")
+						"security.oauth2.resource.userInfoUri:https://example.com")
 				.run();
 		assertFalse(this.context.containsBean("loadBalancedOauth2RestTemplate"));
 	}
@@ -71,7 +71,7 @@ public class ResourceServerTokenRelayAutoConfigurationTests {
 	public void clientConfigured() throws Exception {
 		this.context = new SpringApplicationBuilder(ClientConfiguration.class)
 				.properties("spring.config.name=test", "server.port=0",
-						"security.oauth2.resource.userInfoUri:http://example.com",
+						"security.oauth2.resource.userInfoUri:https://example.com",
 						"security.oauth2.client.clientId=foo")
 				.run();
 		RequestContextHolder.setRequestAttributes(
@@ -82,7 +82,7 @@ public class ResourceServerTokenRelayAutoConfigurationTests {
 		OAuth2RestTemplate template = (OAuth2RestTemplate) ReflectionTestUtils
 				.getField(services, "restTemplate");
 		MockRestServiceServer server = MockRestServiceServer.createServer(template);
-		server.expect(requestTo("http://example.com"))
+		server.expect(requestTo("https://example.com"))
 				.andRespond(withSuccess("{\"id\":\"user\"}", MediaType.APPLICATION_JSON));
 		services.loadAuthentication("FOO");
 		assertEquals("FOO", client.getAccessToken().getValue());
